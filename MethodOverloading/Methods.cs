b06fe12afe367465e2f7aedfdf503ac0ac3ac7c8
April Gilmer <aprilgilmer1@gmail.com>
2023-10-21 22:04:07 -0500

    Method Overloading Exercise Complete

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,33 @@
+﻿namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool withDollars)
+        {
+            if (withDollars && (x + y) != 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else if (withDollars && (x + y) == 1)
+            {
+                return $"{x + y} dollar";
+            }
+            else
+            {
+                return $"{x + y}";
+            }
+        }
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = Methods.Add(1,0,true);
+            Console.WriteLine(answer);
         }
     }
 }
